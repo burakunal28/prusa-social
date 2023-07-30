9879cd54a06ec1fd439b0b075b52e4aadddf88ca
burakunal28 <burakuenal28@gmail.com>
2023-07-30 17:58:03 +0300

    new pages added

---
+++ assets/js/snippets.js
@@ -0,0 +1,20 @@
+const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+const appendAlert = (message, type) => {
+  const wrapper = document.createElement('div')
+  wrapper.innerHTML = [
+    `<div class="alert alert-${type} alert-dismissible font-weight-bolder" role="alert">`,
+    `   <div>${message}</div>`,
+    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+    '</div>'
+  ].join('')
+
+  alertPlaceholder.append(wrapper)
+}
+
+const alertTrigger = document.getElementById('liveAlertBtn')
+if (alertTrigger) {
+  alertTrigger.addEventListener('click', () => {
+    appendAlert('Succesfully posted!', 'success')
+  })
+}
+
